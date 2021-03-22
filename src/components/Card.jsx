8c03306567aca14b718371dc0bc49d8d9f716179
furkan <furkanmavili@outlook.com>
2021-03-22 23:38:04 +0300

    Add card bottom component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,28 +6,8 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import AddAlertOutlinedIcon from "@material-ui/icons/AddAlertOutlined";
-import PaletteOutlinedIcon from "@material-ui/icons/PaletteOutlined";
-import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
-import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
-import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
-import { IconButton, Popper, Tooltip } from "@material-ui/core";
-
-const colors = [
-  "#442f19",
-  "#5b2245",
-  "#42275e",
-  "#1e3a5f",
-  "#16504b",
-  "#345920",
-  "#202124",
-  "#614a19",
-  "#635d19",
-  "#5c2b29",
-  "#3c3f43",
-  "#2d555e",
-];
-
+import ModalCard from "./ModalCard";
+import CardBottom from "./CardBottom";
 const useStyles = makeStyles((theme) => ({
   root: {
     marginBottom: theme.spacing(2),
@@ -72,91 +52,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function ColorPalette({ handleCurrentColor }) {
-  const classes = useStyles();
-  return (
-    <div className={classes.circleWrapper}>
-      {colors.map((item, index) => (
-        <div
-          key={index}
-          onClick={() => handleCurrentColor(item)}
-          className={classes.circle}
-          style={{ backgroundColor: item }}
-        ></div>
-      ))}
-    </div>
-  );
-}
-function CardBottomButton({ label, placement, popper, icon, title }) {
-  const classes = useStyles();
-  const [anchorEl, setanchorEl] = React.useState(null);
-  const handleColor = (event) => {
-    setanchorEl(anchorEl ? null : event.currentTarget);
-  };
-  const open = Boolean(anchorEl);
-  const id = open ? label : undefined;
-  return (
-    <>
-      <Tooltip title={title}>
-        <IconButton
-          aria-describedby={id}
-          onClick={handleColor}
-          className={classes.smallIcon}
-          aria-label="Color"
-        >
-          {icon}
-        </IconButton>
-      </Tooltip>
-      <Popper id={id} open={open} anchorEl={anchorEl} placement={placement}>
-        <div className={classes.paper}>{popper}</div>
-      </Popper>
-    </>
-  );
-}
-function CardBottom({ handleCurrentColor }) {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.bottomMenu}>
-      <CardBottomButton
-        icon={<AddAlertOutlinedIcon />}
-        popper={<h2>hello</h2>}
-        label="color-popper"
-        handleCurrentColor={handleCurrentColor}
-        placement="bottom-start"
-        title="Remind me"
-      />
-
-      <CardBottomButton
-        icon={<PaletteOutlinedIcon />}
-        popper={<ColorPalette handleCurrentColor={handleCurrentColor} />}
-        label="color-popper"
-        handleCurrentColor={handleCurrentColor}
-        placement="top-start"
-        title="Color"
-      />
-      <Tooltip title="Add Image">
-        <IconButton className={classes.smallIcon} aria-label="Add Image">
-          <ImageOutlinedIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="Archive">
-        <IconButton className={classes.smallIcon} aria-label="Archive">
-          <ArchiveOutlinedIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="More">
-        <IconButton className={classes.smallIcon} aria-label="More">
-          <MoreVertOutlinedIcon />
-        </IconButton>
-      </Tooltip>
-    </div>
-  );
-}
-
-export default function CustomCard({ title, content, color, image }) {
+export default function CustomCard({ item }) {
   const classes = useStyles();
   const [showBottom, setShowBottom] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const { title, content, color, image } = item;
   const [currentColor, setCurrentColor] = useState("");
   const handleCurrentColor = (c) => {
     setCurrentColor(c);
@@ -165,35 +65,56 @@ export default function CustomCard({ title, content, color, image }) {
     setCurrentColor(color);
   }, [color]);
   return (
-    <Card
-      className={classes.root}
-      style={{ backgroundColor: currentColor ? currentColor : "inherit" }}
-      onMouseEnter={() => setShowBottom(true)}
-      onMouseLeave={() => setShowBottom(false)}
-    >
-      <CardActionArea disableRipple disableTouchRipple>
-        {image && (
-          <CardMedia
-            component="img"
-            alt={title}
-            height="140"
-            image={image}
-            title={title}
-          />
-        )}
-        <CardContent>
-          <Typography gutterBottom variant="subtitle2" component="h2">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {content}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <>
+      <ModalCard
+        item={item}
+        open={showModal}
+        setOpen={(value) => setShowModal(value)}
+        currentColor={currentColor}
+      />
+      <Card
+        className={classes.root}
+        style={{ backgroundColor: currentColor ? currentColor : "inherit" }}
+        onMouseEnter={() => setShowBottom(true)}
+        onMouseLeave={() => setShowBottom(false)}
+      >
+        <CardActionArea
+          onClick={() => setShowModal(true)}
+          disableRipple
+          disableTouchRipple
+        >
+          {image && (
+            <CardMedia
+              component="img"
+              alt={title}
+              height="140"
+              image={image}
+              title={title}
+            />
+          )}
+          <CardContent>
+            {title && (
+              <Typography gutterBottom variant="subtitle2" component="h2">
+                {title}
+              </Typography>
+            )}
+            {content && (
+              <Typography
+                style={{ wordBreak: "break-all" }}
+                variant="body2"
+                color="textSecondary"
+                component="p"
+              >
+                {content}
+              </Typography>
+            )}
+          </CardContent>
+        </CardActionArea>
 
-      <CardActions style={{ minHeight: 58 }}>
-        {showBottom && <CardBottom handleCurrentColor={handleCurrentColor} />}
-      </CardActions>
-    </Card>
+        <CardActions style={{ minHeight: 58 }}>
+          {showBottom && <CardBottom handleCurrentColor={handleCurrentColor} />}
+        </CardActions>
+      </Card>
+    </>
   );
 }
