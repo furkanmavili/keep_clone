0d7a6a4370927a36311395a29e5235dd3c45b970
furkan <furkanmavili@outlook.com>
2021-03-22 23:38:31 +0300

    Add modal card

---
+++ src/components/ModalCard.jsx
@@ -0,0 +1,140 @@
+import React, { useContext, useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { InputBase, makeStyles } from "@material-ui/core";
+import CardBottom from "./CardBottom";
+import { UserContext } from "../providers/UserProvider";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    border: "1px solid #5f6368",
+    borderRadius: theme.shape.borderRadius,
+    transition: "all .3s ease",
+    width: 600,
+  },
+  flex: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  bottomMenu: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    marginLeft: -10,
+    justifyContent: "space-between",
+  },
+  smallIcon: {
+    "& svg": {
+      fontSize: 18,
+    },
+  },
+  paper: {
+    padding: theme.spacing(1),
+    backgroundColor: "#202124",
+    borderRadius: theme.shape.borderRadius,
+  },
+  circleWrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    maxWidth: 120,
+    gap: 2,
+  },
+  circle: {
+    cursor: "pointer",
+    width: 25,
+    height: 25,
+    borderRadius: "50%",
+    transition: "all 500ms ease",
+    "&:hover": {
+      border: "1px solid #fff",
+    },
+  },
+  titleInput: {
+    fontSize: 20,
+  },
+}));
+
+export default function ModalCard({ open, setOpen, item, currentColor }) {
+  const { title, content, color, image } = item;
+  const classes = useStyles();
+  const [newTitle, setNewTitle] = useState("");
+  const [newContent, setNewContent] = useState("");
+  const [newColor, setNewColor] = useState("");
+
+  console.log(newContent, newContent.split("\n").length);
+  useEffect(() => {
+    setNewTitle(title);
+    setNewContent(content);
+    setNewColor(currentColor);
+  }, [title, content, currentColor]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleSubmit = () => {
+    if (title !== newTitle || content !== newContent) {
+      // const result = addNote(title, content, currentColor, user.uid);
+      console.log("submitting...", newTitle, newContent);
+    }
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+        className={classes.backdrop}
+        PaperProps={{
+          className: classes.root,
+          style: { backgroundColor: newColor ? newColor : "#202124" },
+          elevation: 5,
+        }}
+        scroll="paper"
+      >
+        <DialogTitle id="form-dialog-title">
+          <InputBase
+            autoFocus
+            margin="dense"
+            id="newTitle"
+            label="Title"
+            placeholder="Title"
+            type="text"
+            fullWidth
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+            className={classes.titleInput}
+          />
+        </DialogTitle>
+        <DialogContent>
+          <InputBase
+            autoFocus
+            multiline
+            margin="dense"
+            id="newContent"
+            label="Content"
+            placeholder="Note"
+            type="text"
+            fullWidth
+            value={newContent}
+            onChange={(e) => setNewContent(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <CardBottom
+            closeButton={
+              <Button variant="text" onClick={handleSubmit} color="default">
+                Cancel
+              </Button>
+            }
+            handleCurrentColor={(c) => setNewColor(c)}
+          />
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
