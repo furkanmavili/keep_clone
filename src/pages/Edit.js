519b97b54e7840f064ac7577b36fc409647d61b2
furkan <furkanmavili@outlook.com>
2021-04-04 00:52:34 +0300

    Add mock pages

---
+++ src/pages/Edit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Edit() {
+  return <div>edit page</div>;
+}
+
+export default Edit;
---
+++ src/pages/Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Search() {
+  return <div>Hi</div>;
+}
+
+export default Search;
