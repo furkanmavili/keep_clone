db71191724326249d2413617b4e03097053be5d5
furkan <furkanmavili@outlook.com>
2021-04-04 00:55:04 +0300

    Take user info from upside

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,27 +1,25 @@
 import Card from "../components/Card";
 import MasonryGrid from "../components/MasonryGrid";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getNotes } from "../firebase";
-import { UserContext } from "../providers/UserProvider";
 
-function CardList() {
+function CardList({ user }) {
   const [notes, setNotes] = useState([]);
-  const user = useContext(UserContext);
   const ref = getNotes(user);
-
   useEffect(() => {
-    if (!ref) return;
-    ref.onSnapshot((querySnapshot) => {
-      const items = [];
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        data["docID"] = doc.id;
-        items.push(data);
+    if (ref) {
+      ref.onSnapshot((querySnapshot) => {
+        const items = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          data["docID"] = doc.id;
+          items.push(data);
+        });
+        setNotes(items);
       });
-      setNotes(items);
-    });
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [user]);
 
   return (
     <MasonryGrid>
