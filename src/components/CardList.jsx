56da34180074d9c948a21b88db1485de47e57ec5
furkan <furkanmavili@outlook.com>
2021-04-25 12:43:37 +0300

    take notes from parent

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,29 +1,8 @@
 import Card from "../components/Card";
 import MasonryGrid from "../components/MasonryGrid";
-import React, { useContext, useEffect, useState } from "react";
-import { getNotes } from "../firebase";
-import { UserContext } from "../providers/UserProvider";
+import React from "react";
 
-function CardList({ filterCallback }) {
-  const [notes, setNotes] = useState([]);
-  const user = useContext(UserContext);
-  useEffect(() => {
-    const ref = getNotes(user);
-    if (ref) {
-      ref.onSnapshot((querySnapshot) => {
-        const items = [];
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          data["docID"] = doc.id;
-          items.push(data);
-        });
-        const filtered = items.filter(filterCallback);
-        setNotes(filtered);
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
-
+function CardList({ notes }) {
   return (
     <MasonryGrid>
       {notes.map((item, index) => {
