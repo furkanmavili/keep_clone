ce9fbd7db0783f55788579ab336cc087724a4ae4
furkanmavili <furkanmavili@outlook.com>
2021-05-31 08:50:27 +0300

    Add reminder page

--- src/pages/Reminders.js
+++ src/pages/Reminders.js
@@ -1,7 +1,34 @@
 import React from "react";
+import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneOutlined";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    height: "80%",
+    textAlign: "center",
+  },
+  largeIcon: {
+    width: 120,
+    height: 120,
+  },
+}));
 
 function Reminders() {
-  return <h1>Reminders Page</h1>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div>
+        <NotificationsNoneOutlinedIcon color="disabled" className={classes.largeIcon} />
+      </div>
+      <Typography variant="h5" color="textSecondary">
+        Notes with upcoming reminders appear here
+      </Typography>
+    </div>
+  );
 }
 
 export default Reminders;
