d7f74303ebeb225e403f409a2db7e677823b028d
furkanmavili <furkanmavili@outlook.com>
2021-05-31 08:52:31 +0300

    Add redirection to input

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
-// import CloseIcon from "@material-ui/icons/Close";
+import { useHistory, useLocation } from "react-router";
+import clsx from "clsx";
+import CloseIcon from "@material-ui/icons/Close";
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: "2px 4px",
@@ -15,12 +17,16 @@ const useStyles = makeStyles((theme) => ({
   },
   input: {
     marginLeft: theme.spacing(1),
-    color: "#e8eaed",
     flex: 1,
+    color: "inherit",
+  },
+  focusedInput: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.common.black,
   },
   iconButton: {
     padding: 10,
-    color: "#e8eaed",
+    color: "inherit",
   },
   divider: {
     height: 28,
@@ -30,9 +36,31 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CustomizedInputBase() {
   const classes = useStyles();
-
+  const history = useHistory();
+  const location = useLocation();
+  const [inputValue, setInputValue] = useState("");
+  const [inputFocused, setInputFocused] = useState(false);
+  const handleFocus = () => {
+    setInputFocused(true);
+    history.push("/search");
+  };
+  const handleClose = () => {
+    history.push("/");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(location.pathname + "?text=" + inputValue);
+  };
   return (
-    <Paper component="form" className={classes.root}>
+    <Paper
+      component="form"
+      onSubmit={handleSubmit}
+      className={clsx(classes.root, {
+        [classes.focusedInput]: inputFocused,
+      })}
+      onFocus={handleFocus}
+      onBlur={() => setInputFocused(false)}
+    >
       <IconButton className={classes.iconButton} aria-label="menu">
         <SearchIcon />
       </IconButton>
@@ -40,14 +68,13 @@ export default function CustomizedInputBase() {
         className={classes.input}
         placeholder="Search"
         inputProps={{ "aria-label": "search" }}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      {/* <IconButton
-        type="submit"
-        className={classes.iconButton}
-        aria-label="search"
-      >
+
+      <IconButton onClick={handleClose} className={classes.iconButton} aria-label="search">
         <CloseIcon />
-      </IconButton> */}
+      </IconButton>
     </Paper>
   );
 }
